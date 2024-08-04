d4d6fdb9467c70421c9d8681f57d379e7a69723b
0xPrakharG <prakhargoyal.3014@gmail.com>
2024-08-04 15:49:41 +0530

    push change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   optimizeDeps: {
-    include: ['sonner'],
+    include: ["sonner"],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        charset: false,
+      },
+    },
   },
   build: {
+    minify: false,
     chunkSizeWarningLimit: 1000,
   },
 });
